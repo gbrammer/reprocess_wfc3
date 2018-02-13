93323b45011e0190ad0f4856a11b1d9b3be7b873
Gabriel Brammer <brammer@stsci.edu>
2018-02-13 15:43:32 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='reprocess_wfc3',
+      version='0.1',
+      description='Reprocessing scripts for WFC3/IR',
+      author='Gabriel Brammer',
+      author_email='brammer@stsci.edu',
+      url='https://github.com/gbrammer/reprocess_wfc3/',
+      packages=['reprocess_wfc3'],
+     )
